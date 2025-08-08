3fe3639f5fe2b8ee6bb51bbed692bc3995201b3f
Matheus Teixeira Pires <matheus.teteu.tp.72@gmail.com>
2025-08-08 19:37:52 -0300

    adição da transação para criar veículo

--- chaincode/txdefs/createNewVehicle.go
+++ chaincode/txdefs/createNewVehicle.go
@@ -3,10 +3,9 @@ package txdefs
 import (
 	"encoding/json"
 
+	"github.com/hyperledger-labs/cc-tools-demo/chaincode/datatypes"
 	"github.com/hyperledger-labs/cc-tools/accesscontrol"
 	"github.com/hyperledger-labs/cc-tools/assets"
-	""
-	
 	"github.com/hyperledger-labs/cc-tools/errors"
 	sw "github.com/hyperledger-labs/cc-tools/stubwrapper"
 	tx "github.com/hyperledger-labs/cc-tools/transactions"
@@ -73,9 +72,41 @@ var CreateNewVehicle = tx.Transaction{
 	
 	Routine: func(sw *sw.StubWrapper, req map[string]interface{}) ([]byte, errors.ICCError) {
 		id := req["id"].(string)
-		vehicleType := req["vehicleType"].(vehicleType)
+		vehicleType := req["vehicleType"].(datatypes.VehicleType)
+		ownerID := req["owner"].(string)
+		model := req["model"].(string)
+		mark := req["mark"].(string)
+		year := req["year"].(string)
+		color := req["color"].(string)
 
+		vehicleMap := make(map[string]interface{})
+		vehicleMap["@assetType"] = "vehicle"
+		vehicleMap["id"] = id
+		vehicleMap["vehicleType"] = vehicleType
+		vehicleMap["ownerID"] = ownerID
+		vehicleMap["model"] = model
+		vehicleMap["mark"] = mark
+		vehicleMap["year"] = year
+		vehicleMap["color"] = color
 
+		vehicleAsset, err := assets.NewAsset(vehicleMap)
 		
+		if err != nil {
+			return nil, errors.WrapError(err, "Failed to create a new asset")
+		}
+
+		_, err = vehicleAsset.PutNew(sw)
+
+		if err != nil {
+			return nil, errors.WrapErrorWithStatus(err, "Error saving asset on blockchain", err.Status())
+		}
+
+		vehicleJSON, nerr := json.Marshal(vehicleAsset)
+
+		if nerr != nil {
+			return nil, errors.WrapError(nil, "failed to encode asset to JSON format")
+		}
+
+		return vehicleJSON, nil
 	},
 }
